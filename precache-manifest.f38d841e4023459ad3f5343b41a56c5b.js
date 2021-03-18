75d72f2296e36f7b9f365fbee346f43cc0e5f061
ta-david-yu <d366095@gmail.com>
2021-03-18 22:00:32 +0100

    Updates

--- precache-manifest.f38d841e4023459ad3f5343b41a56c5b.js
+++ precache-manifest.f38d841e4023459ad3f5343b41a56c5b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "06395abf41c85035bfa146196fbb672b",
-    "url": "/about-me/static/media/m6x11.06395abf.woff"
+    "revision": "4b67f4cae7b165eb8df707021f06dccf",
+    "url": "/about-me/static/media/m6x11.4b67f4ca.woff2"
   },
   {
-    "revision": "7977f765327ac11717da",
+    "revision": "c3ff4817dc783b7b7b00",
     "url": "/about-me/static/css/main.524721a8.chunk.css"
   },
   {
@@ -16,24 +16,24 @@ self.__precacheManifest = [
     "url": "/about-me/static/js/2.72c11ca5.chunk.js"
   },
   {
+    "revision": "06395abf41c85035bfa146196fbb672b",
+    "url": "/about-me/static/media/m6x11.06395abf.woff"
+  },
+  {
+    "revision": "cc8dfaa3938829493e45c4a13b6629c3",
+    "url": "/about-me/static/media/m6x11.cc8dfaa3.ttf"
+  },
+  {
+    "revision": "c3ff4817dc783b7b7b00",
+    "url": "/about-me/static/js/main.d359228b.chunk.js"
+  },
+  {
     "revision": "b99dd26c57ef102981d6905fe699d79a",
     "url": "/about-me/static/media/m5x7.b99dd26c.woff"
   },
   {
     "revision": "1681d9b97eb6ced79230094129e0c5b3",
     "url": "/about-me/static/media/m5x7.1681d9b9.woff2"
-  },
-  {
-    "revision": "7977f765327ac11717da",
-    "url": "/about-me/static/js/main.5973ca4a.chunk.js"
-  },
-  {
-    "revision": "4b67f4cae7b165eb8df707021f06dccf",
-    "url": "/about-me/static/media/m6x11.4b67f4ca.woff2"
-  },
-  {
-    "revision": "cc8dfaa3938829493e45c4a13b6629c3",
-    "url": "/about-me/static/media/m6x11.cc8dfaa3.ttf"
   },
   {
     "revision": "ea30f28d8ad67b8eef6be486e3637b98",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
     "url": "/about-me/static/media/m5x7.78e46408.svg"
   },
   {
-    "revision": "7b495f9b65342c1ebc4287fd9b4259d3",
+    "revision": "0b575837f98f6301b5d7efeb287ebfd4",
     "url": "/about-me/index.html"
   }
 ];
